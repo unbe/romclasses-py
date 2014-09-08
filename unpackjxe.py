bff0fb3cc036cf6967123f1584f1d0ed7a0b572a
unbe@github <unbe@users.noreply.github.com>
2014-09-09 00:16:22 +0200

    interfaces

--- unpackjxe.py
+++ unpackjxe.py
@@ -6,10 +6,42 @@ input = open(sys.argv[1], "rb")
 data = input.read()
 input.close()
 
-def RelativePointer(name):
-	return ExprAdapter(Struct(name, Anchor("_base"), SLInt32("_ptr")), 
-		encoder = lambda obj, ctx: "",
-		decoder = lambda obj, ctx: obj._base + obj._ptr)
+def deco(obj, ctx):
+	if obj._ptr == 0:
+		print "Obj: " + repr(obj)
+		print "Ctx: " + repr(ctx)
+	return obj._base + obj._ptr
+
+def Relative(name):
+	return ExprAdapter(Struct(name, Anchor("base"), SLInt32("ptr")), 
+		encoder = None,
+		decoder = lambda obj, ctx: obj.base + obj.ptr if obj.ptr != 0 else None)
+		#decoder = deco)
+
+def Carp(ptr, ctx, struct):
+	addr = ptr(ctx)
+	if addr is None:
+		print "None: " + repr(ctx)
+		print struct
+		return 0
+	return addr
+		
+
+def MaybePointer(ptr, struct):
+	return If(lambda ctx: ptr(ctx) is not None, Pointer(ptr, struct))
+
+def StringPointer(ptr, name):
+	return MaybePointer(ptr, PascalString(name, length_field=ULInt16("length")))
+
+def StringRef(name):
+	return ExprAdapter(
+		Struct(name, 
+			Relative("ptr"), 
+			StringPointer(lambda ctx: ctx.ptr, "str")
+		),
+		encoder = None,
+		decoder = lambda obj, ctx: obj.str)
+	
 
 J9ROMImageHeader = Struct("J9ROMImageHeader",
 	Enum(ULInt32("signature"),
@@ -19,28 +51,28 @@ J9ROMImageHeader = Struct("J9ROMImageHeader",
 	ULInt32("flags_and_version"),
 	ULInt32("rom_size"),
 	ULInt32("class_count"),
-	RelativePointer("jxe_pointer"),
-	RelativePointer("toc_pointer"),
-	RelativePointer("first_class_pointer"),
-	RelativePointer("aot_pointer"),
+	Relative("jxe_pointer"),
+	Relative("toc_pointer"),
+	Relative("first_class_pointer"),
+	Relative("aot_pointer"),
 	Array(16, ULInt8("symbol_file_id")),
 	Pointer(lambda ctx: ctx.toc_pointer, Array(lambda ctx: ctx.class_count, 
 		Struct("J9ROMClassTOCEntry",
-			RelativePointer("class_name_pointer"),
-			RelativePointer("class_pointer"),
-			Pointer(lambda ctx: ctx.class_name_pointer, PascalString("class_name", length_field=ULInt16("length"))),
-			Pointer(lambda ctx: ctx.class_pointer, Struct("J9ROMClass",
+			Relative("class_name_pointer"),
+			Relative("class_pointer"),
+			StringPointer(lambda ctx: ctx.class_name_pointer, "class_name"),
+			MaybePointer(lambda ctx: ctx.class_pointer, Struct("J9ROMClass",
 			    ULInt32("rom_size"),
 			    ULInt32("single_scalar_static_count"),
-			    RelativePointer("class_name_pointer"),
-			    RelativePointer("superclass_name_pointer"),
+			    Relative("class_name_pointer"),
+			    Relative("superclass_name_pointer"),
 			    ULInt32("modifiers"),
 			    ULInt32("interface_count"),
-			    RelativePointer("interfaces_pointer"),
+			    Relative("interfaces_pointer"),
 			    ULInt32("rom_method_count"),
-			    RelativePointer("rom_methods_pointer"),
+			    Relative("rom_methods_pointer"),
 			    ULInt32("rom_field_count"),
-			    RelativePointer("rom_fields_pointer"),
+			    Relative("rom_fields_pointer"),
 			    ULInt32("object_static_count"),
 			    ULInt32("double_scalar_static_count"),
 			    ULInt32("ram_constant_pool_count"),
@@ -48,17 +80,18 @@ J9ROMImageHeader = Struct("J9ROMImageHeader",
 			    ULInt32("crc"),
 			    ULInt32("instance_size"),
 			    ULInt32("instance_shape"),
-			    RelativePointer("cp_shape_description_pointer"),
-			    RelativePointer("outer_class_name_pointer"),
+			    Relative("cp_shape_description_pointer"),
+			    Relative("outer_class_name_pointer"),
 			    ULInt32("member_access_flags"),
 			    ULInt32("inner_class_count"),
-			    RelativePointer("inner_classes_pointer"),
+			    Relative("inner_classes_pointer"),
 			    ULInt16("major_version"),
 			    ULInt16("minor_version"),
 			    ULInt32("optional_flags"),
-			    RelativePointer("optional_info_pointer"),
-				Pointer(lambda ctx: ctx.class_name_pointer, PascalString("class_name", length_field=ULInt16("length"))),
-				Pointer(lambda ctx: ctx.superclass_name_pointer, PascalString("superclass_name", length_field=ULInt16("length"))),
+			    Relative("optional_info_pointer"),
+				MaybePointer(lambda ctx: ctx.class_name_pointer, PascalString("class_name", length_field=ULInt16("length"))),
+				MaybePointer(lambda ctx: ctx.superclass_name_pointer, PascalString("superclass_name", length_field=ULInt16("length"))),
+				MaybePointer(lambda ctx: ctx.interfaces_pointer, Array(lambda ctx: ctx.interface_count, StringRef("interface"))),
 			)),
 		)
 	)),
